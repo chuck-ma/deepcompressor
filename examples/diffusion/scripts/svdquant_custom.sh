b0a88e3bf2c4460e47043c2248d3de87b2f4096c
chuckma <chuck_ma@qq.com>
2024-11-11 17:11:37 +0800

    add task_type support

--- examples/diffusion/scripts/svdquant_custom.sh
+++ examples/diffusion/scripts/svdquant_custom.sh
@@ -2,4 +2,4 @@
 pip install -e .
 
 cd examples/diffusion
-python -m deepcompressor.app.diffusion.ptq configs/model/flux.1-custom.yaml configs/svdquant/int4.yaml
+HF_ENDPOINT="https://hf-mirror.com" HF_HOME="/root/autodl-tmp/huggingface" MODELSCOPE_CACHE="/root/autodl-tmp/modelscope/hub" python -m deepcompressor.app.diffusion.ptq configs/model/flux.1-custom.yaml configs/svdquant/int4.yaml
