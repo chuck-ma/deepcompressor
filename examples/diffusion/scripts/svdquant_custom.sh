62cb77636488863af567c19bafa50dde8397f080
chuckma <chuck_ma@qq.com>
2024-11-11 17:58:18 +0800

    add task_type support

--- examples/diffusion/scripts/svdquant_custom.sh
+++ examples/diffusion/scripts/svdquant_custom.sh
@@ -2,4 +2,18 @@
 pip install -e .
 
 cd examples/diffusion
-HF_ENDPOINT="https://hf-mirror.com" HF_HOME="/root/autodl-tmp/huggingface" MODELSCOPE_CACHE="/root/autodl-tmp/modelscope/hub" python -m deepcompressor.app.diffusion.ptq configs/model/flux.1-custom.yaml configs/svdquant/int4.yaml
+
+### Step 1: Evaluation Baselines Preparation
+
+HF_ENDPOINT="https://hf-mirror.com" HF_HOME="/root/autodl-tmp/huggingface" MODELSCOPE_CACHE="/root/autodl-tmp/modelscope/hub" python -m deepcompressor.app.diffusion.ptq configs/model/flux.1-custom.yaml --output-dirname reference
+
+
+### Step 2: Calibration Dataset Preparation
+
+HF_ENDPOINT="https://hf-mirror.com" HF_HOME="/root/autodl-tmp/huggingface" MODELSCOPE_CACHE="/root/autodl-tmp/modelscope/hub" python -m deepcompressor.app.diffusion.dataset.collect.calib \
+    configs/model/flux.1-custom.yaml configs/collect/qdiff.yaml
+
+
+### Step 3: Smooth Quantization
+
+HF_ENDPOINT="https://hf-mirror.com" HF_HOME="/root/autodl-tmp/huggingface" MODELSCOPE_CACHE="/root/autodl-tmp/modelscope/hub" python -m deepcompressor.app.diffusion.ptq configs/model/flux.1-custom.yaml configs/svdquant/int4.yaml --save-model /root/autodl-tmp/flux.1-custom-svdquant-int4
