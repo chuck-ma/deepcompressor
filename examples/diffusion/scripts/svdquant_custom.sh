89ee7395ccfce011a5a144e1d4754557168082d8
chuckma <chuck_ma@qq.com>
2024-11-12 10:15:55 +0800

    add task_type support

--- examples/diffusion/scripts/svdquant_custom.sh
+++ examples/diffusion/scripts/svdquant_custom.sh
@@ -1,14 +1,9 @@
 # 如果代码有改动，需要重新安装
 pip install -e .
 
-cd examples/diffusion
+# cd examples/diffusion
 
-### Step 0: Environment Preparation
-#### 下载 MJHQ-30K 数据集
-HF_ENDPOINT="https://hf-mirror.com" HF_HOME="/root/autodl-tmp/huggingface" MODELSCOPE_CACHE="/root/autodl-tmp/modelscope/hub" python -c '
-from datasets import load_dataset
-dataset = load_dataset("playgroundai/MJHQ-30K")
-'
+
 ### Step 1: Evaluation Baselines Preparation
 
  cd examples/diffusion && HF_ENDPOINT="https://hf-mirror.com" HF_HOME="/root/autodl-tmp/huggingface" MODELSCOPE_CACHE="/root/autodl-tmp/modelscope/hub" python -m deepcompressor.app.diffusion.ptq configs/model/flux.1-custom.yaml --output-dirname reference
